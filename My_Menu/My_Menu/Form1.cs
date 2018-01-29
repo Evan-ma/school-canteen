4d65b81ba8b5f4b114ac6ee9472c72ca601b68a0
Evan-ma <35055954+Evan-ma@users.noreply.github.com>
2018-01-29 20:05:33 +0800

    Add files via upload
    上传李金澎的菜单代码。

---
+++ My_Menu/My_Menu/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_Menu
+{
+    public partial class Form1 : Form
+    {
+        public Form1(Form2 form2)
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            new Form3(this).Show();
+            this.Hide();
+        }
+    }
+}
---
+++ My_Menu/My_Menu/Form2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_Menu
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text=="123")
+            {
+                //MessageBox.Show("登陆成功！", "登陆成功");
+                new Form1(this).Show();
+                this.Hide();
+            }
+            else MessageBox.Show("密码错误，请重新输入！", "登陆出错");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
