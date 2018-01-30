b69a27a3c9e49e3bceea4d0bed4f708d218b59ad
wangzhiqiang <wangzhiqiang@neusoft.edu.cn>
2018-01-30 11:35:22 +0800

    调整菜单大小。

--- BrushFaceSystem/CanteenTerminal/MainMenu.Designer.cs
+++ BrushFaceSystem/CanteenTerminal/MainMenu.Designer.cs
@@ -39,9 +39,9 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button1.Location = new System.Drawing.Point(51, 75);
+            this.button1.Location = new System.Drawing.Point(110, 86);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 62);
+            this.button1.Size = new System.Drawing.Size(234, 152);
             this.button1.TabIndex = 0;
             this.button1.Text = "刷脸支付";
             this.button1.UseVisualStyleBackColor = true;
@@ -49,9 +49,9 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button2.Location = new System.Drawing.Point(249, 75);
+            this.button2.Location = new System.Drawing.Point(363, 86);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(150, 62);
+            this.button2.Size = new System.Drawing.Size(234, 152);
             this.button2.TabIndex = 1;
             this.button2.Text = "用户管理";
             this.button2.UseVisualStyleBackColor = true;
@@ -59,9 +59,9 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button3.Location = new System.Drawing.Point(457, 75);
+            this.button3.Location = new System.Drawing.Point(616, 86);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(150, 62);
+            this.button3.Size = new System.Drawing.Size(234, 152);
             this.button3.TabIndex = 2;
             this.button3.Text = "余额充值";
             this.button3.UseVisualStyleBackColor = true;
@@ -70,9 +70,9 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button4.Location = new System.Drawing.Point(51, 236);
+            this.button4.Location = new System.Drawing.Point(110, 301);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(150, 62);
+            this.button4.Size = new System.Drawing.Size(234, 152);
             this.button4.TabIndex = 3;
             this.button4.Text = "账目管理";
             this.button4.UseVisualStyleBackColor = true;
@@ -80,9 +80,9 @@
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button5.Location = new System.Drawing.Point(249, 236);
+            this.button5.Location = new System.Drawing.Point(363, 301);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(150, 62);
+            this.button5.Size = new System.Drawing.Size(234, 152);
             this.button5.TabIndex = 4;
             this.button5.Text = "数据管理";
             this.button5.UseVisualStyleBackColor = true;
@@ -90,9 +90,9 @@
             // button6
             // 
             this.button6.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button6.Location = new System.Drawing.Point(457, 236);
+            this.button6.Location = new System.Drawing.Point(616, 301);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(150, 62);
+            this.button6.Size = new System.Drawing.Size(234, 152);
             this.button6.TabIndex = 5;
             this.button6.Text = "系统设置";
             this.button6.UseVisualStyleBackColor = true;
@@ -101,7 +101,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(662, 375);
+            this.ClientSize = new System.Drawing.Size(981, 565);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
